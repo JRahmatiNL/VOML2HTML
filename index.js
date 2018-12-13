2c453a1f3225a33ea9b275036e214d7d80283f77
Jafar <jafar.bij.ict@gmail.com>
2018-12-13 20:39:19 +0100

    Add index.css & extract GetHtml4VOResouce from UpdateUI in index.js

--- index.js
+++ index.js
@@ -1,77 +1,44 @@
+// Note that you need some basic understanding of regular expressions to understand this code.
+// You can use the interactive tutorial on https://regexone.com/ to quickly learn the basics.
+//
+// In short:
+// - the GetHtml4VOResouce method further in the code uses regex to convert vo resource code to html
+// - anything between parantheses (e.g. '(' & ')') represents a group
+// - \s stands for space characters
+// - * stands for 0 or more, while + stands for 1 or more 
+// - \w stands for word characters (including alphanumeric words)
+// - \d stands for digit characters
+// - /expression/ followed by optional special chars is a common way to represent regular expressions
+// - https://regex101.com/ can be used for pattern visualisation
+// 
 function UpdateUI()
 {
-    // Note that you need some basic understanding of regular expressions to understand this code.
-    // You can use the interactive tutorial on https://regexone.com/ to quickly learn the basics.
-    //
-    // In short:
-    // - anything between parantheses (e.g. '(' & ')') represents a group
-    // - \s stands for space characters
-    // - * stands for 0 or more, while + stands for 1 or more 
-    // - \w stands for word characters (including alphanumeric words)
-    // - \d stands for digit characters
-    // - /expression/ followed by optional special chars is a common way to represent regular expressions
-    // - https://regex101.com/ can be used for pattern visualisation
-    // 
-    var myRegex = /\s*CONTROL\s"(.*?)",\s*(\w+),.*?(\d+), (\d+), (\d+), (\d+)/;
     var uiPanel = document.getElementById("UIPanel");
     var text = document.getElementById("codeArea").value;
     var textParts = text.split(/(\r*\n)/g);
-    var htmlResult = "";
+    uiPanel.innerHTML = "";
     for (let index = 0; index < textParts.length; index++) {
         const element = textParts[index];
-        /*
-        Group1: controls text value
-        Group2: controls (serialized) name
-        Group3 until 6: x, y & probably width & height coordinates
-        */
-        var groupValues = myRegex.exec(element);
-        if(groupValues != null)
-        {
-            var controlName = groupValues[2];
-            console.log("control detected " + controlName);
-            var controlsTextValue = groupValues[1];;
-            var controlXPos = groupValues[3];
-            var controlYPos = groupValues[4];
-            var controlWidth = groupValues[5];
-            var controlHeight = groupValues[6];
-            var htmlStyleDisplayText = (
-                "display:block;" +
-                "position: absolute;" +
-                "font-size: 8pt;"
-            );
-            var htmlStylePositionText = (
-                "left: " + controlXPos + "px;" +
-                "top: " + controlYPos + "px;"
-            );
-            var htmlStyleSizeText = (
-                "width: " + controlWidth + "px;" + 
-                "height: " + controlHeight + "px;"
-            );
-            var controlStyleText = (
-                "border-style:solid;background-color:silver;" + 
-                htmlStyleDisplayText +
-                htmlStylePositionText +
-                htmlStyleSizeText
-            );
-            var descriptionStyleSizeText = (
-                "width: auto;" + 
-                "height: auto;"
-            );
-            var descriptionStylePositionText = (
-                "left: " + controlXPos + "px;" +
-                "top: " + (Number(controlYPos) + Number(controlHeight) - 2) + "px;"
-            );
-            var controlDescriptionStyleText = (
-                "border-style:dotted;background-color:white;" + 
-                htmlStyleDisplayText +
-                descriptionStylePositionText +
-                descriptionStyleSizeText
-            );
-            htmlResult += (
-                '<div id="'+controlName+'" title="'+controlName+'" style="' + controlStyleText + '" />' + controlsTextValue + '</div>'
-            );
-        }
+        uiPanel.innerHTML += GetHtml4VOResouce(element);
     }
-    console.log("htmlResult: " + htmlResult);
-    uiPanel.innerHTML = htmlResult;
+}
+
+function GetHtml4VOResouce(element)
+{
+    var myRegex = /\s*CONTROL\s"(.*?)",\s*(\w+),.*?(\d+), (\d+), (\d+), (\d+)/;
+    var groupValues = myRegex.exec(element);
+    if(groupValues == null) return "";
+    
+    var groupIndex = 1;
+    var controlsTextValue = groupValues[groupIndex++];
+    var controlName = groupValues[groupIndex++];
+    var controlStyleText = ([
+        `left: ${groupValues[groupIndex++]}px`, `top: ${groupValues[groupIndex++]}px`,
+        `width: ${groupValues[groupIndex++]}px`, `height: ${groupValues[groupIndex++]}px`,
+    ].join(";"));
+    var controlAttributes = [
+        `id="${controlName}"`, `title="${controlName}"`,
+        `class="vocontrol" style="${controlStyleText}"`
+    ].join(" ");
+    return (`<div ${controlAttributes} />${controlsTextValue}</div>`);
 }
